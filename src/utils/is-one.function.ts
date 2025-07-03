da488fe438ab7aa17c36d5ed5bfd889d0b943df7
Krzysztof Pająk <chris.araneo@gmail.com>
2025-07-03 21:11:31 +0200

    feat: helper functions

---
+++ src/utils/is-one.function.ts
@@ -0,0 +1,2 @@
+// eslint-disable-next-line @typescript-eslint/no-magic-numbers
+export const isOne = (value: number): boolean => value === 1;
---
+++ src/utils/is-zero.function.ts
@@ -0,0 +1,2 @@
+// eslint-disable-next-line @typescript-eslint/no-magic-numbers
+export const isZero = (value: number): boolean => value === 0;
