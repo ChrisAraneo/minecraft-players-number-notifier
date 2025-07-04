cff79765b54438601a01cc1b59a3cf5adba93035
Krzysztof Pająk <chris.araneo@gmail.com>
2025-07-04 20:33:10 +0200

    feat: util sliceTwo function

---
+++ src/utils/slice-two.function.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-magic-numbers */
+/* eslint-disable @typescript-eslint/no-unsafe-type-assertion */
+
+import { isArray } from "lodash";
+
+export const sliceTwo = <T>(array: T[]): [T, ...T[]] | [] => {
+  if (isArray(array) && array.length >= 2) {
+    return array.slice(2) as [T, ...T[]];
+  }
+
+  return [];
+}
