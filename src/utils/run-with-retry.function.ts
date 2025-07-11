a5bdc0bfbad8f3e1d733f1f0ba08553c22bb6834
Krzysztof Pająk <chris.araneo@gmail.com>
2025-07-11 21:40:09 +0200

    feat(run-with-retry): add error param to errorFn

--- src/utils/run-with-retry.function.ts
+++ src/utils/run-with-retry.function.ts
@@ -2,13 +2,13 @@ import { isFunction } from 'lodash';
 
 export const runWithRetry = async <T>(
   fn: () => Promise<T>,
-  errorFn?: () => void,
+  errorFn?: (error: unknown) => void,
 ): Promise<T> => {
   try {
     return await fn();
-  } catch {
+  } catch(error: unknown) {
     if (isFunction(errorFn)) {
-      errorFn();
+      errorFn(error);
     }
   }
 
