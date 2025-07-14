4196adcfa252c1f3b66cfd5df291bc5cd78f3ebf
Krzysztof Pająk <chris.araneo@gmail.com>
2025-07-14 19:49:34 +0200

    feat(is-log-level-or-throw): use TypeError

--- src/utils/is-log-level-or-throw.function.ts
+++ src/utils/is-log-level-or-throw.function.ts
@@ -1,12 +1,14 @@
 import { LogLevel } from '@chris.araneo/logger';
 import { isString } from 'lodash';
 
-export const isLogLevelOrThrow = (
+export const isLogLevelOrThrow: (
+  value: unknown,
+) => asserts value is LogLevel = (
   value: unknown,
 ): asserts value is LogLevel => {
   if (isString(value) && ['debug', 'info', 'warn', 'error'].includes(value)) {
     return;
   }
 
-  throw new Error('Invalid log level');
+  throw new TypeError('Invalid log level');
 };
