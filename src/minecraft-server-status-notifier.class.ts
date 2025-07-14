f7f804e7bed3da438f99e66e9bc1f92935302997
Krzysztof Pająk <chris.araneo@gmail.com>
2025-07-14 19:56:11 +0200

    refactor(minecraft-server-status-notifier): fix eslint issues, extract private methods

--- src/minecraft-server-status-notifier.class.ts
+++ src/minecraft-server-status-notifier.class.ts
@@ -1,8 +1,18 @@
+/* eslint-disable @typescript-eslint/no-magic-numbers */
+
 import { CurrentDirectory, FileSystem } from '@chris.araneo/file-system';
 import { HealthCheckService } from '@chris.araneo/health-check';
-import { Logger, LogLevel } from '@chris.araneo/logger';
+import { Logger } from '@chris.araneo/logger';
 import dashify from 'dashify';
-import { get, isEmpty, isNull, isNumber, isUndefined, set } from 'lodash';
+import {
+  get,
+  isArray,
+  isEmpty,
+  isNull,
+  isNumber,
+  isUndefined,
+  set,
+} from 'lodash';
 import fetch from 'node-fetch-native';
 import {
   catchError,
@@ -28,67 +38,95 @@ import { ServerStatus } from './models/server-status.interface';
 import { EnvironmentVariables } from './process/environment-variables.class';
 import { Process } from './process/process.class';
 import { Store } from './store/store.class';
+import { isLogLevelOrThrow } from './utils/is-log-level-or-throw.function';
+import { isStringArrayOrThrow } from './utils/is-string-array-or-throw.function';
 
 export class MinecraftServerStatusNotifier {
+  private readonly store = new Store();
+
+  private process!: Process;
   private logger!: Logger;
   private config!: Config;
   private discordApiClient: DiscordApiClient | null = null;
   private apiClient!: ServerStatusApiClient;
-  private readonly store = new Store();
 
   async initialize(): Promise<void> {
-    const process = new Process();
-    const currentDirectory = new CurrentDirectory();
-    const fileSystem = new FileSystem();
+    await this.loadConfiguration();
 
-    const configLoader = new ConfigLoader(currentDirectory, fileSystem);
-    this.config =
-      (await firstValueFrom(configLoader.readConfigFile()).catch(
-        (error: unknown) => new Logger('error').error(String(error)),
-      )) ?? {};
-
-    const environmentVariables = new EnvironmentVariables(process).get();
+    const environmentVariables = this.loadEnvironmentVariables();
 
     if (environmentVariables.CI) {
       return;
     }
 
-    this.loadEnvironmentVariables(environmentVariables);
-
-    if (isEmpty(Object.keys(this.config))) {
-      new Logger('error').error('No config');
+    if (!this.validateConfiguration()) {
       return;
     }
 
-    this.logger = new Logger(this.config['log-level'] as LogLevel);
-    this.logger.info('Minecraft Players Number Notifier v0.5.3');
-    this.logger.debug(`Loaded configuration: ${JSON.stringify(this.config)}`);
-
+    this.initializeLogger();
     this.initializeDiscordClient();
-    this.apiClient = new ServerStatusApiClient(this.config, this.logger, fetch);
+    this.initializeApiClient();
 
     this.startHealthCheckService(environmentVariables);
-    this.startMonitoring();
-    this.subscribeToNotifications();
+    this.startPollingServers();
+    this.subscribeToServerStatusesChanges();
   }
 
-  private loadEnvironmentVariables(
-    environmentVariables: Record<string, unknown>,
-  ): void {
+  private async loadConfiguration(): Promise<void> {
+    this.process = new Process();
+    const currentDirectory = new CurrentDirectory();
+    const fileSystem = new FileSystem();
+    const configLoader = new ConfigLoader(currentDirectory, fileSystem);
+
+    const config = await firstValueFrom(configLoader.readConfigFile()).catch(
+      (error: unknown) => new Logger('error').error(String(error)),
+    );
+
+    if (config) {
+      this.config = config;
+    }
+  }
+
+  private validateConfiguration(): boolean {
+    if (isEmpty(Object.keys(this.config))) {
+      new Logger('error').error('No config');
+      return false;
+    }
+    return true;
+  }
+
+  private initializeLogger(): void {
+    const logLevel = this.config['log-level'];
+
+    isLogLevelOrThrow(logLevel);
+
+    this.logger = new Logger(logLevel);
+    this.logger.info('Minecraft Players Number Notifier v0.5.3');
+    this.logger.debug(`Loaded configuration: ${JSON.stringify(this.config)}`);
+  }
+
+  private initializeApiClient(): void {
+    this.apiClient = new ServerStatusApiClient(this.config, this.logger, fetch);
+  }
+
+  private loadEnvironmentVariables(): Record<
+    string,
+    string | string[] | undefined
+  > {
+    const environmentVariables = new EnvironmentVariables(this.process).get();
+
     for (const [key, value] of Object.entries(environmentVariables)) {
       if (!isUndefined(value)) {
         set(this.config, dashify(key).split('_').join('-'), value);
       }
     }
+
+    return environmentVariables;
   }
 
   private initializeDiscordClient(): void {
-    const token: string | null = (this.config['discord-token'] || null) as
-      | string
-      | null;
-    const predefinedRecipients = (this.config.recipients || []) as
-      | string
-      | string[];
+    const token = this.config['discord-token'] || null;
+    const predefinedRecipients = this.config.recipients || [];
 
     if (Boolean(this.config.discord) && isNull(token)) {
       this.logger.error('Token is null');
@@ -100,9 +138,9 @@ export class MinecraftServerStatusNotifier {
         ? new DiscordApiClient(
             this.config,
             this.logger,
-            Array.isArray(predefinedRecipients)
+            isArray(predefinedRecipients)
               ? [...predefinedRecipients]
-              : [predefinedRecipients],
+              : [String(predefinedRecipients)],
           )
         : null;
   }
@@ -111,7 +149,7 @@ export class MinecraftServerStatusNotifier {
     environmentVariables: Record<string, unknown>,
   ): void {
     if (environmentVariables.MPNN_HEALTH_CHECK_PORT) {
-      new HealthCheckService(
+      void new HealthCheckService(
         '/health',
         Number(environmentVariables.MPNN_HEALTH_CHECK_PORT),
         this.logger,
@@ -119,15 +157,19 @@ export class MinecraftServerStatusNotifier {
     }
   }
 
-  private startMonitoring(): void {
+  private startPollingServers(): void {
+    const servers = this.config.servers || [];
+
+    isStringArrayOrThrow(servers);
+
     interval(Number(this.config.interval))
       .pipe(
-        mergeMap(() => from((this.config.servers as string[]) || [])),
+        mergeMap(() => from(servers)),
         tap((server: string) => {
           const playerCount = this.apiClient.getPlayerCount(server);
           const getListOfPlayerNames = this.apiClient.getPlayers(server);
 
-          firstValueFrom(
+          void firstValueFrom(
             playerCount.pipe(
               this.logNumberOfPlayers(server),
               this.logPlayerNames(getListOfPlayerNames, server),
@@ -144,7 +186,7 @@ export class MinecraftServerStatusNotifier {
       .subscribe();
   }
 
-  private subscribeToNotifications(): void {
+  private subscribeToServerStatusesChanges(): void {
     this.store
       .getServerStatuses()
       .pipe(
@@ -159,19 +201,24 @@ export class MinecraftServerStatusNotifier {
       .subscribe((status) => this.sendNotifications(status));
   }
 
-  private logNumberOfPlayers(server: string) {
+  private logNumberOfPlayers(
+    server: string,
+  ): OperatorFunction<
+    NumberOfOnlinePlayersResult,
+    NumberOfOnlinePlayersResult
+  > {
     return tap((result: NumberOfOnlinePlayersResult) => {
-      if (!isNumber(result?.online)) {
+      if (!isNumber(result.online)) {
         this.logger.info(
           `Could not read number of players on server ${server}.`,
         );
       } else if (result.online === 1) {
         this.logger.info(
-          `Server ${server} has currently: ${result?.online} player.`,
+          `Server ${server} has currently: ${result.online} player.`,
         );
       } else {
         this.logger.info(
-          `Server ${server} has currently: ${result?.online} players.`,
+          `Server ${server} has currently: ${result.online} players.`,
         );
       }
     });
@@ -184,12 +231,9 @@ export class MinecraftServerStatusNotifier {
     return mergeMap((onlineResult: NumberOfOnlinePlayersResult) =>
       getListOfPlayerNames.pipe(
         tap((playersResult) => {
-          if (
-            playersResult.success &&
-            (playersResult?.players || []).length > 0
-          ) {
+          if (playersResult.success && !isEmpty(playersResult.players)) {
             this.logger.info(
-              `Players online: ${(playersResult?.players || [])
+              `Players online: ${(playersResult.players ?? [])
                 .map((player) => player.name)
                 .join(', ')}`,
             );
@@ -206,17 +250,17 @@ export class MinecraftServerStatusNotifier {
             players: get(result, 'players', []),
           });
         }),
-        map((result) => result?.players || []),
+        map((result) => result.players ?? []),
       ),
     );
   }
 
   private sendNotifications(status: ServerStatus): void {
     if (this.discordApiClient) {
-      this.discordApiClient.sendMessage(
+      void this.discordApiClient.sendMessage(
         status.server,
         status.online,
-        status.players || [],
+        status.players ?? [],
       );
     }
   }
